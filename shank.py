61116c8415bfa295d3c763d5a13e225819702105
Jere Mäkinen <jere.makinen@tuni.fi>
2021-02-27 12:21:36 +0200

    Ratkaisualgoritmia muokattu tehokkaammaksi ja tulostuksia muokattu

--- shank.py
+++ shank.py
@@ -1,20 +1,18 @@
-import numpy as np
 import math
 import time
 
 PRIMES = [331777, 16785407, 54018521, 214748357, 342999977, 788748341, 2147483647,
           6547483043, 16148168401, 80630964769, 1171432692373]
 
-GENERATORS = [5, 5, 6, 2, 3, 2, 7,
-              2, 22, 17, 2]
+GENERATORS = [5, 5, 6, 2, 3, 2, 7, 2, 22, 17, 2]
 
-X = [math.floor(331777/2), math.floor(16785407/2), math.floor(54018521/2), math.floor(214748357/2),
-     math.floor(342999977/2), math.floor(788748341/2), math.floor(2147483647/2),
-     math.floor(6547483043/2), math.floor(16148168401/2), math.floor(80630964769/2), math.floor(1171432692373/2)]
+MAX_RUNNING_TIME = 7200
 
-H = [331776, 16785406, 54018520, 214748356, 342999976, 788748340, 2147483646,
-     6547483042, 16148168400, 80630964768, 1171432692372]
+H = 2021
 
+# Tarkistaa alkion (g)
+
+# Checks element's (g) order in set integers modulo p
 def checkOrder(p,g):
     new_value = 1
     for i in range(1,p):
@@ -23,12 +21,14 @@ def checkOrder(p,g):
             return i
     return 0
 
+
 def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
+
 
 def modinv(a, m):
     g, x, y = egcd(a, m)
@@ -37,6 +37,10 @@ def modinv(a, m):
     else:
         return x % m
 
+
+# Etsii pienimmän mahdollisen virittäjän joukolle kokonaisluvut modulo p.
+
+# Find the smallest possible generator for integers modulo p, where p is a prime number
 def findSmallestGenerator(p):
     g = 2
     while g < p:
@@ -46,63 +50,100 @@ def findSmallestGenerator(p):
         g += 1
     return 0
 
+
+# Shankin algoritmi. Ratkaisee yhtälön h = p^x mod p, missä p on alkuluku
+# Palauttaa ratkaisun x, tehtyjen vertailuiden lukumäärän ja suoritusajan.
+
+# Shank's algorithm. Solves the equation h = p^x mod p, where p is a prime number.
+# Returns the solution x, the amount of made comparisons and the running time of the algorithm.
 def solveDLP_shank(p, g, h):
     n = 1 + math.floor(math.sqrt(p-1))
     start_time = time.time()
-    u = modinv(g, p)**n % p
-    L1 = np.array([g**0])
-    L2 = np.array([h])
+    u = pow(g, p-2, p)**n % p
+    l1 = []
+    l2 = []
+    l1.append(g**0)
+    l2.append(h)
     new_g = 1
     new_u = u
-    for i in range(1,n+1):
-        L1 = np.append(L1, (g*new_g) % p)
+    for i in range(1, n+1):
+        l1.append((g*new_g) % p)
         new_g = (g*new_g) % p
-        L2 = np.append(L2, (h*new_u) % p)
+        l2.append((h*new_u) % p)
         new_u = (u*new_u) % p
-    for i in range(0, L1.size):
-        for j in range(0, L2.size):
-            if L1[i] == L2[j]:
+    comparison_amount = 0
+    for i in range(0, len(l1)):
+        for j in range(0, len(l2)):
+            comparison_amount += 1
+            if l1[i] == l2[j]:
                 print('Solution is ', i+j*n)
                 run_time = time.time() - start_time
                 print("--- %s seconds ---" % run_time)
-                return [int(i+j*n), run_time]
+                return [int(i+j*n), comparison_amount,  run_time]
+            if time.time() - start_time > MAX_RUNNING_TIME:
+                print('Solving took too much time')
+                return [-1, comparison_amount, MAX_RUNNING_TIME]
 
 
 def solveDLP_bruteforce(p, g, h):
     start_time = time.time()
     solution = 0
     new_g = 1
-    for i in range(1,p):
+    comparison_amount = 0
+    for i in range(1, p):
+        comparison_amount += 1
         if g*new_g % p == h:
             solution = i
             break
+        if time.time() - start_time > MAX_RUNNING_TIME:
+            print('Solving took too much time')
+            return [-1, comparison_amount, MAX_RUNNING_TIME]
         new_g = g * new_g % p
     print('Solution is', solution)
     run_time = time.time() - start_time
     print("--- %s seconds ---" % run_time)
-    return [int(solution), run_time]
+    return [int(solution), comparison_amount, run_time]
+
+
+def give_primes():
+    return [PRIMES, GENERATORS]
 
 
 def main():
-    file_shank = open('results_shank.txt', 'a')
-    file_bruteforce = open('results_bruteforce.txt', 'a')
-    for i in range(0, len(PRIMES)):
+    file_shank = open('results_shank.csv', 'a')
+    file_bruteforce = open('results_bruteforce.csv', 'a')
+    file_check_result = open('results_check.csv', 'a')
+    primes_and_generators = give_primes()
+    primes = primes_and_generators[0]
+    generators = primes_and_generators[1]
+    for i in range(0, len(primes)):
         print("")
         print("Shank's algorithm:")
-        solution_shank = solveDLP_shank(PRIMES[i], GENERATORS[i], H[i])
+        solution_shank = solveDLP_shank(primes[i], generators[i], H)
         print("")
         print("Brute force:")
-        solution_bruteforce = solveDLP_bruteforce(PRIMES[i], GENERATORS[i], H[i])
+        solution_bruteforce = solveDLP_bruteforce(primes[i], generators[i], H)
         start_time = time.time()
         print("")
-        print('Check result (shank): ', pow(GENERATORS[i], solution_shank[0], PRIMES[i]))
-        print('Check result (brute force): ', pow(GENERATORS[i], solution_bruteforce[0], PRIMES[i]))
-        print("--- %s seconds ---" % (time.time() - start_time))
-        file_shank.write(str(PRIMES[i]) + ';' + str(solution_shank[0]) + ';' + str(solution_shank[1]) + '\n')
-        file_bruteforce.write(str(PRIMES[i]) + ';' + str(solution_bruteforce[0]) + ';' + str(solution_bruteforce[1]) +
-                              '\n')
+        if solution_shank[2] < MAX_RUNNING_TIME:
+            print('Check result (shank): ', pow(generators[i], solution_shank[0], primes[i]))
+        else:
+            print("Shank's algorithm ran out of time.")
+
+        if solution_bruteforce[2] < MAX_RUNNING_TIME:
+            print('Check result (brute force): ', pow(generators[i], solution_bruteforce[0], primes[i]))
+        else:
+            print("Brute force run out of time.")
+        check_time = time.time() - start_time
+        print("--- %s seconds ---" % check_time)
+        file_shank.write(str(primes[i]) + ';' + str(solution_shank[0]) + ';' + str(solution_shank[1]) + ';' +
+                             str(solution_shank[2]) + '\n')
+        file_bruteforce.write(str(primes[i]) + ';' + str(solution_bruteforce[0]) + ';' +
+                              str(solution_bruteforce[1]) + ';' + str(solution_bruteforce[2]) + '\n')
+        file_check_result.write(str(primes[i]) + ';' + str(solution_shank[0]) + ';' + str(check_time))
     file_shank.close()
     file_bruteforce.close()
+    file_check_result.close()
 
 
 main()
